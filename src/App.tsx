cb64eb8004325f2a59a4be01ffd783a6de024081
Vishal Thakur <vishal.thakur@celebaltech.com>
2024-02-23 18:21:30 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Button colorScheme='blue'>Button</Button>
+    </>
+  )
+}
+
+export default App
