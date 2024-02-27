a68ae0518d768852fe9787ca14b999c3ef544908
Vishal Thakur <vishal.thakur@celebaltech.com>
2024-02-27 08:15:24 +0530

    Added grid structure in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
  
 
   return (
     <>
-     <Button colorScheme='blue'>Button</Button>
+     <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`
+     }} >
+      <GridItem area={'nav'} bg={'green'}>Nav</GridItem>
+      <Show above='lg'>
+        <GridItem area={'aside'} bg={'blue'}>Aside</GridItem>
+      </Show>
+      <GridItem area={'main'} bg={'red'}>Main</GridItem>
+     </Grid>
     </>
   )
 }
