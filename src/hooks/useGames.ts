67963617ef4215dae24c39457553ce4054afef1d
Vishal Thakur <vishal.thakur@celebaltech.com>
2024-03-02 01:11:00 +0530

    Displaying platform icons || installed 'reacticons' || added 'PlatformIconList.tsx' file in components folder || added platformiconlist component in 'GameCard.tsx' || added parent_platform and PLatform interface in 'userGames.ts' file.

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,10 +2,25 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image: string;
+    parent_platforms: {platform: Platform}[];
+    /*
+     parent_platforms: [{platform: {id: 1, name: "PC", slug: "pc"}},…]
+        0: {platform: {id: 1, name: "PC", slug: "pc"}}
+            platform: {id: 1, name: "PC", slug: "pc"}
+                id: 1
+                name: "PC"
+                slug: "pc"
+    */
 }
 interface FetchGameResponse {
     count: number;
