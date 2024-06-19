5985e9fe3306ea531173e996d5fe48104d43a3ed
Vishal Thakur <vishal.thakur@celebaltech.com>
2024-06-19 18:22:23 +0530

    Genres Cleanup

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
+import { Button, HStack, Heading, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url';
 import GameListSkeleton from './GameListSkeleton';
@@ -15,16 +15,19 @@ const GenreList = ({onSelectedGenre, selectedGenre}: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
 
   return (
-    <List>
-      {isLoading && skeletons.map((skeleton) => <GameListSkeleton key={skeleton}/>)}
-      {datas.map(genre => 
-      <ListItem key={genre.id} paddingY={'5px'}>
-        <HStack>
-          <Image boxSize={'32px'} borderRadius={8} src={getCroppedImageUrl(genre.image_background)}/>
-          <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectedGenre(genre)} fontSize={'lg'} variant={'link'}>{genre.name}</Button>
-        </HStack>
-      </ListItem>)}
-    </List>
+    <>
+      <Heading fontSize={'2xl'} marginBottom={3}>Genres</Heading>
+      <List>
+        {isLoading && skeletons.map((skeleton) => <GameListSkeleton key={skeleton}/>)}
+        {datas.map(genre => 
+        <ListItem key={genre.id} paddingY={'5px'}>
+          <HStack>
+            <Image boxSize={'32px'} borderRadius={8} objectFit={'cover'} src={getCroppedImageUrl(genre.image_background)}/>
+            <Button whiteSpace={'normal'} textAlign={'left'} fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} onClick={() => onSelectedGenre(genre)} fontSize={'lg'} variant={'link'}>{genre.name}</Button>
+          </HStack>
+        </ListItem>)}
+      </List>
+    </>
   )
 }
 
